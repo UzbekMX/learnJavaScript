7d01d99d408e5b1396f29278a8cb03a0dcf097ed
unknown <gear.misha.mx@gmail.com>
2026-03-02 13:37:13 +0200

    calculator operation if

--- assets/script/script.js
+++ assets/script/script.js
@@ -4,41 +4,52 @@ const value = parseInt(inputOne[0].value) + parseInt(inputTwo[0].value)
 
 const inputValueOne = document.querySelector(".value1")
 inputValueOne.addEventListener('input', (event) => {
-    console.log("numberOne", event.target.valueAsNumber)
+    "numberOne", event.target.valueAsNumber
 })
 
 const inputValueTwo = document.querySelector(".value2")
 inputValueTwo.addEventListener('input', (event) => {
-    console.log("numberTwo", event.target.valueAsNumber)
+    "numberTwo", event.target.valueAsNumber
 })
 
-let btnSum = document.querySelector(".calc-btn")
+const btnSum = document.querySelector(".calc-btn")
 btnSum.addEventListener("click", () => {
-    result.value = plus
+    result.value = sum  
+    if (operation == "+"){
+    }
+    if(operation == "-"){
+    }
+    if(operation == "*"){
+    }
+    if(operation == "/"){
+    }
 })
+
+let operation = undefined
+let sum = undefined
 
 let plus = document.querySelector(".btn-plus")
 plus.addEventListener("click", function () {
-    plus = parseInt(inputValueOne.value) + parseInt(inputValueTwo.value)
-    console.log(result.value = plus)
+    operation = "+"
+    // sum = parseInt(inputValueOne.value) + parseInt(inputValueTwo.value)
 })
 
 let minus = document.querySelector(".btn-minus")
 minus.addEventListener("click", function () {
-    minus = parseInt(inputValueOne.value) - parseInt(inputValueTwo.value)
-    console.log(result.value = minus)
+    operation = "-"
+    sum = parseInt(inputValueOne.value) - parseInt(inputValueTwo.value)
 })
 
 let multiply = document.querySelector(".btn-multiply")
 multiply.addEventListener("click", function () {
-    multiply = parseInt(inputValueOne.value) * parseInt(inputValueTwo.value)
-    console.log(result.value = multiply)
+    operation = "*"
+    sum = parseInt(inputValueOne.value) * parseInt(inputValueTwo.value)
 })
 
 let divide = document.querySelector(".btn-divide")
 divide.addEventListener("click", function () {
-    divide = parseInt(inputValueOne.value) / parseInt(inputValueTwo.value)
-    console.log(result.value = divide)
+    operation = "/"
+    sum = parseInt(inputValueOne.value) / parseInt(inputValueTwo.value)
 })
 
 const result = document.querySelector(".result")
